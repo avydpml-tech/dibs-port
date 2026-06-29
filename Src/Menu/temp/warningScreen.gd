68aae9026c09535d5d9350531f285c6ee737ddab
avydpml-tech <avydpml@gmail.com>
2026-06-29 13:33:13 +0600

    Update warningScreen.gd

--- Src/Menu/temp/warningScreen.gd
+++ Src/Menu/temp/warningScreen.gd
@@ -8,48 +8,32 @@ export (String) var scene_path_to_load_3 = "res://Src/1_World/1_zones/ship/Stage
 onready var _debug_label = null
 
 func _ready():
-	# Создаём лейбл для отладки на экране
 	var label = Label.new()
 	label.rect_position = Vector2(10, 10)
-	label.rect_size = Vector2(1260, 200)
+	label.rect_size = Vector2(1260, 100)
 	label.autowrap = true
 	label.add_color_override("font_color", Color(1, 0, 0))
 	add_child(label)
 	_debug_label = label
-
-	_debug_label.text = "Ready OK. Globals: " + str(Globals != null)
-
+	_debug_label.text = "Ready OK"
 	$message/Label.set_text(Globals.game_version)
 	Pause.set_allow_pause(false)
 
-func _show_error(msg):
-	if _debug_label:
-		_debug_label.text = "ERROR: " + msg
-
 func _process(delta):
 	if Input.is_action_just_pressed("ui_enter") or Input.is_action_just_pressed("ui_t") or Input.is_action_just_pressed("ui_touch"):
-		_debug_label.text = "Touched! is_entered_mainhub: " + str(Globals.is_entered_mainhub)
 		if Globals.is_entered_mainhub:
 			Globals.is_show_mainhub_start_screen = true
-			_debug_label.text = "Loading mainhub..."
+			_debug_label.text = "Going to mainhub..."
 			get_node("/root/SceneChanger")._change_scene(mainhub.get_path())
 		else:
-			if scene_path_to_load == null:
-				_show_error("scene_path_to_load is NULL!")
-				return
-			_debug_label.text = "Loading: " + str(scene_path_to_load.get_path())
-			get_node("/root/SceneChanger")._change_scene(scene_path_to_load.get_path())
+			_debug_label.text = "Going to MainHub directly..."
+			get_node("/root/SceneChanger")._change_scene("res://Src/1_World/1_zones/MainHub/Stage-Mainhub.tscn")
 
 	elif Input.is_action_just_pressed("ui_esc"):
 		get_tree().quit()
 
 func _on_quick_start_pressed():
 	get_node("/root/SceneChanger")._change_scene(scene_path_to_load_3)
-	$message/quick_start.release_focus()
-
-func _on_debug_pressed():
-	player_skipped()
-	get_node("/root/SceneChanger")._change_scene("res://Src/1_World/debugArena.tscn")
 	$message/quick_start.release_focus()
 
 func _on_warehouse_pressed():
